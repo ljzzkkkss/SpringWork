10eac7f648520b591fe115a27ab77b2cf04307fb
ljzzkkkss <793958447@qq.com>
2016-08-30 10:15:11 +0800

    new ways

--- src/main/java/com/excelib/dao/BaseDao.java
+++ src/main/java/com/excelib/dao/BaseDao.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcOperations;
 
 import javax.annotation.Resource;
 import java.util.List;
